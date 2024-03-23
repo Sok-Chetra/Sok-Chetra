a73192e4415df0e576225e819aeffe843f17b33c
Sok-Chetra <trasok9090@gmail.com>
2024-03-23 20:41:16 +0700

    builded nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,20 +4,20 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.pinimg.com',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'c4.wallpaperflare.com',
-        port: '',
-      },
-    ],
-  },
+   images: {
+      remotePatterns: [
+         {
+            protocol: 'https',
+            hostname: 'i.pinimg.com',
+            port: '',
+         },
+         {
+            protocol: 'https',
+            hostname: 'c4.wallpaperflare.com',
+            port: '',
+         },
+      ],
+   },
 };
 
 export default withNextIntl(nextConfig);
