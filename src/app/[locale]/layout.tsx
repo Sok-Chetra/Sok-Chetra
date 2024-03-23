af5cfdb49168eae836c0a721e0fba897244a7c06
Sok-Chetra <63409230+Sok-Chetra@users.noreply.github.com>
2024-03-23 23:02:48 +0700

    update something

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -29,7 +29,7 @@ export async function generateStaticParams() {
    return ['en', 'kh'].map((locale) => ({ locale }))
  }
 
- export const dynamic = 'dynamic force';
+ export const dynamic = 'force-dynamic';
 
 export default async function LocaleLayout({
    children,
