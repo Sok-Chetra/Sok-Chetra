d79600a9bead9f69c43fc8f648eb86443317590c
Sok-Chetra <63409230+Sok-Chetra@users.noreply.github.com>
2024-03-23 21:45:28 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,8 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   output: "standalone",
-   basePath: process.env.NODE_ENV === "production" ? "" : undefined,
-   experimental: {
-      appDir: true,
-   },
-   images: {
-      unoptimized: true,
-   },
-   reactStrictMode: true,
+   output: "export",
+   basePath: '/sok_chetra',
 };
 
 export default withNextIntl(nextConfig);
